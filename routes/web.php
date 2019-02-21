693fe22f131eaf118e051fc7a70167a6c3f085ce
Nick Davies <nickdavies07@outlook.com>
2019-02-21 12:01:54 +0000

    Update posts resource in route middleware auth

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::resource('posts', PostController::class);
+Route::group(['middleware' => 'auth'], function () {
+	Route::get('/home', 'HomeController@index')->name('home');
+	Route::resource('posts', PostController::class);
+});
